ddc3eebcc5a9dbecd4b71f7cba9bd51a2f85adf6
Ellyn Golden <ejgolden94@gmail.com>
2021-02-08 12:39:05 -0500

    refactoring for all game logic to exist in game object

--- connect_four/js/app.js
+++ connect_four/js/app.js
@@ -1,187 +1,183 @@
 $(()=>{
-  //global variables
-  const turns = [
-    {
-      team: 'red',
-      color: '#C91818',
-      streak: 0
-    },
-    {
-      team: 'blue',
-      color: '#1874C9',
-      streak: 0
-    }
-  ]
-  let currentTurn = turns[1]
-  // store the game results here
-  let game = []
-  /// controls whether or not moves can be made
-  let playing = true
-  let currentMoves = 0
-  let maxMoves
-
-  /////////////////////////////////
-  ////Functions to create the board
-  /////////////////////////////////
-  const makeNewSlot = (row,col) =>{
-    const $newSlot = $('<div>').addClass(`slot ${row} ${col}`)
-    const $newHole = $('<div>').addClass(`hole ${row} ${col}`).attr('id',`${row}-${col}`).on('click',checkValidMove)
-    $('.game-container').append($newSlot.append($newHole))
-    return $newSlot
-  }
-
-  const generateBoard = (row,col) => {
-    maxMoves = row*col
-    for(i=0;i<row;i++){
-      game.push([])
-      for(j=0;j<col;j++){
-        game[i].push(j) 
-        makeNewSlot(i,j).css('width',`${900/col}px`).css('height',`${900/col}px`)
-      }
-    }
-  }
-
-  /////////////////////////////////////
-  ////Functions to determine the winner
-  /////////////////////////////////////
-  const updateStreak = () => {
-    currentTurn.streak++
-    let teamNo = turns.indexOf(currentTurn) + 1
-    $(`#team-${teamNo}-streak`).text(currentTurn.streak)
-  }
-
-  const win = () => {
-    $('.message').append($('<h2>').text(`${currentTurn.team} wins the game!`).css('color',currentTurn.color))
-    updateStreak()
-    playing = false
-  }
-
-  const checkTie = () => {
-    if (currentMoves === maxMoves){
-      $('.message').append($('<h2>').text(`It's A Tie!`))
-    playing = false
-    }
-  }
-
-  const checkFourInARow = (fourInARow) => {
-    if (fourInARow.length > 4){
-      fourInARow.shift()
-    }
-    if (fourInARow.length === 4 && fourInARow.every(hole => hole === currentTurn.team)){
-      win()
-      return true
-    }
-  }
-
-  const checkRow = (holeRow) => {
-    const fourInARow = []
-    for(hole of game[holeRow]){
-      fourInARow.push(hole)
-      if (checkFourInARow(fourInARow)){
-        break
-      }
-    }
-  }
-
-  const checkColumn = (holeCol) => {
-    const fourInARow = []
-    for(row of game){
-      fourInARow.push(row[holeCol])
-      if (checkFourInARow(fourInARow)){
-        break
-      }
-    }
-  }
-
-  const checkDiagonals = () => {
-    for(col=-2;col<game[0].length+2;col++){
-      const fourInARowForward = []
-      const fourInARowBackward = []
-      let offset=0
-      for(row=0;row<game.length;row++){
-        fourInARowForward.push(game[row][col+offset])
-        fourInARowBackward.push(game[row][col-offset])
-        offset++
-        if(checkFourInARow(fourInARowForward)){
-          break
+  const connectFour = {
+      currentMoves: 0,
+      maxMoves: 0,
+      game: [],
+      playing: true,
+      turns: [],
+      currentTurn: 'none',   
+      addPlayer(player){
+          this.turns.push(player)
+          this.currentTurn = this.turns[0]   
+      },
+      //////////////
+      ////Start Game
+      ///////////////
+      startGame(){
+          connectFour.playing = true
+          $('.game-container').empty()
+          $('.message').empty()
+          connectFour.game = []
+          connectFour.currentMoves = 0
+          connectFour.generateBoard(6,7)
+      },
+      /////////////////////////////////
+      ////Functions to create the board
+      /////////////////////////////////
+      makeNewSlot(row,col){
+          const $newSlot = $('<div>').addClass(`slot ${row} ${col}`)
+          const $newHole = $('<div>').addClass(`available hole ${row} ${col}`).attr('id',`${row}-${col}`).on('click',this.checkValidMove)
+          $('.game-container').append($newSlot.append($newHole))
+          return $newSlot
+      },
+      generateBoard(row,col){
+          this.maxMoves = row * col
+          for(let i=0;i<row;i++){
+              this.game.push([])
+              for(let j=0;j<col;j++){
+                  this.game[i].push(j) 
+                  this.makeNewSlot(i,j).css('width',`${900/col}px`).css('height',`${900/col}px`)
+              }
           }
-        if(checkFourInARow(fourInARowBackward)){
-          break
+      },
+      ////////////////////////////
+      ////Functions to make a move
+      ////////////////////////////
+      checkValidMove(event){
+          // console.log(this.playing) /// why is this undefined .. 
+          // console.log(connectFour.playing)
+          if (connectFour.playing){
+              const hole = $(event.target)
+              const classList = hole.attr('class').split(' ')
+              let holeRow = parseInt(classList[2])
+              let holeCol = parseInt(typeof classList[3] === 'undefined'? classList[2]: classList[3])
+          
+              if((holeRow === connectFour.game.length-1) || (["blue","red"].includes(connectFour.game[holeRow+1][holeCol]))) {
+                  connectFour.placeChip(hole,holeRow,holeCol)
+              }
           }
-        } 
+      },
+      placeChip(hole,holeRow,holeCol){
+          this.currentMoves++
+          this.currentTurn = this.turns[(this.turns.indexOf(this.currentTurn)+1)%this.turns.length]
+          this.game[holeRow][holeCol]= this.currentTurn.team
+          hole.off('click',this.checkValidMove)
+          hole.removeClass('available')
+          this.fallIntoPlace(holeCol)
+          this.checkRow(holeRow)
+          this.checkColumn(holeCol)
+          this.checkDiagonals()
+          this.checkTie()
+      },
+      ///////////////////////
+      /////Falling into place
+      ///////////////////////
+      /* Pseudo code 
+      when a hole is clicked it should set off a function that 
+      1. turns the top hole the color of that team through adding a class
+      2. removes the class from te previous hole and adds it to the next hole 
+      3. 1 and 2 continue until the next hole in the column is filled with a red or blue chip
+      4. humans should be able to see this occurring, so a set time interval should be specified between class changes
+      */
+      fallIntoPlace(holeCol){
+          let startRow = 0
+          const fall = setInterval(()=>{
+          if((startRow === this.game.length-1) || (["blue","red"].includes(this.game[startRow][holeCol]))){
+              clearInterval(fall)
+          }
+              $(`#${startRow-1}-${holeCol}`).removeClass(this.currentTurn.team)
+              $(`#${startRow}-${holeCol}`).addClass(this.currentTurn.team)
+              startRow++
+          },50)
+      },
+      /////////////////////////////////////
+      ////Functions to determine the winner
+      /////////////////////////////////////
+      win(){
+          $('.hole').removeClass('available')
+          $('.message').append($('<h2>').text(`${this.currentTurn.team} wins the game!`).css('color',this.currentTurn.color))
+          this.currentTurn.updateStreak()
+          this.playing = false
+      },
+      checkTie(){
+          if (this.currentMoves === this.maxMoves){
+            $('.message').append($('<h2>').text(`It's A Tie!`))
+            this.playing = false
+          }
+      },
+      checkFourInARow(fourInARow){
+          if (fourInARow.length > 4){
+            fourInARow.shift()
+          }
+          if (fourInARow.length === 4 && fourInARow.every(hole => hole === this.currentTurn.team)){
+            this.win()
+            return true
+          }
+      },
+      checkRow(holeRow){
+          const fourInARow = []
+          for(hole of this.game[holeRow]){
+            fourInARow.push(hole)
+            if (this.checkFourInARow(fourInARow)){
+              break
+            }
+          }
+      },
+      checkColumn(holeCol){
+          const fourInARow = []
+          for(row of this.game){
+            fourInARow.push(row[holeCol])
+            if (this.checkFourInARow(fourInARow)){
+              break
+            }
+          }
+      },
+      checkDiagonals(){
+          for(col=-2;col<this.game[0].length+2;col++){
+              const fourInARowForward = []
+              const fourInARowBackward = []
+              let offset=0
+              for(row=0;row<this.game.length;row++){
+              fourInARowForward.push(this.game[row][col+offset])
+              fourInARowBackward.push(this.game[row][col-offset])
+              offset++
+              if(this.checkFourInARow(fourInARowForward)){
+                  break
+                  }
+              if(this.checkFourInARow(fourInARowBackward)){
+                  break
+                  }
+              } 
+          }
       }
-    }
-  
-  ///////////////////////
-  /////Falling into place
-  ///////////////////////
-  /* Pseudo code 
-  when a hole is clicked it should set off a function that 
-  1. turns the top hole the color of that team through adding a class
-  2. removes the class from te previous hole and adds it to the next hole 
-  3. 1 and 2 continue until the next hole in the column is filled with a red or blue chip
-  4. humans should be able to see this occurring, so a set time interval should be specified between class changes
-  */
-  const fallIntoPlace = (holeCol) => {
-    let startRow = 0
-    const fall = setInterval(()=>{
-      if((startRow === game.length-1) || (["blue","red"].includes(game[startRow][holeCol]))){
-        clearInterval(fall)
-      }
-        $(`#${startRow-1}-${holeCol}`).removeClass(currentTurn.team)
-        $(`#${startRow}-${holeCol}`).addClass(currentTurn.team)
-        startRow++
-    },50)
-    // console.log(startRow)
-    // console.log(game);
   }
 
-  ////////////////////////////
-  ////Functions to make a move
-  ////////////////////////////
-  const placeChip = (hole,holeRow,holeCol) => {
-      currentMoves++
-      currentTurn = turns[(turns.indexOf(currentTurn)+1)%turns.length]
-      game[holeRow][holeCol]= currentTurn.team
-      // hole.css('background-color',currentTurn.color)
-      hole.off('click',checkValidMove)
-      fallIntoPlace(holeCol)
-      checkRow(holeRow)
-      checkColumn(holeCol)
-      checkDiagonals()
-      checkTie()
-  } 
-
-  const checkValidMove = (event) => {
-    if (playing){
-      const hole = $(event.target)
-      const classList = hole.attr('class').split(' ')
-      let holeRow = parseInt(classList[1])
-      let holeCol = parseInt(typeof classList[2] === 'undefined'? classList[1]: classList[2])
-
-      if((holeRow === game.length-1) || (["blue","red"].includes(game[holeRow+1][holeCol]))) {
-        placeChip(hole,holeRow,holeCol)
+  class Player {
+      constructor(team,color,teamNo) {
+          this.team = team 
+          this.color = color
+          this.teamNo = teamNo
+          this.streak = 0
       }
-    }
+      updateStreak(){
+          this.streak++
+          $(`#team-${this.teamNo}-streak`).text(this.streak)
+      }
   }
 
-  ////////////////////////////
-  ////Staring the Game
-  ////////////////////////////
-  const startGame = () => {
-    playing = true
-    $('.game-container').empty()
-    $('.message').empty()
-    game = []
-    currentMoves = 0
-    generateBoard(6,7)
-  }
+  ///Create 2 players
+  const player1 = new Player('red','#C91818',1)
+  const player2 = new Player('blue','#1874C9',2)
 
+  ///Add players to the game
+  connectFour.addPlayer(player1)
+  connectFour.addPlayer(player2)
 
-  /// Event Listeners 
-  const $startbtn = $('#start').on('click',startGame)
+  //Event Listeners
+  const $startbtn = $('#start').on('click',connectFour.startGame)
 
+  // create the initial board
+  connectFour.generateBoard(6,7)
+  console.log(connectFour)
 
-  /// Initial Start 
-  generateBoard(6,7)
 })
